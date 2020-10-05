d618081fb1200d795f21245e32b4ac38eb45d5bc
David Mendoza <drmendoz@espol.edu.ec>
2020-10-05 05:13:14 -0500

    Cambios en el chat

--- lib/screens/pedidoActual/components/views/chat.dart
+++ lib/screens/pedidoActual/components/views/chat.dart
@@ -14,152 +14,174 @@ import 'package:izi_repartidores/size_config.dart';
 
 class Chat extends StatefulWidget {
   Orden ordenActual;
-  Chat({Key key,this.ordenActual}) : super(key: key);
+  Chat({Key key, this.ordenActual}) : super(key: key);
 
   @override
   _ChatState createState() => _ChatState();
 }
 
 class _ChatState extends State<Chat> {
-  TextEditingController mensajeController= TextEditingController();
-  List<Mensaje> mensajes=[];
+  TextEditingController mensajeController = TextEditingController();
+  List<Mensaje> mensajes = [];
 
   @override
   void initState() {
     this.getMensajesNow();
-    Timer.periodic(Duration(seconds: 5),getMensajes);
+    Timer.periodic(Duration(seconds: 3), getMensajes);
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: EdgeInsets.only(top:getProportionateScreenHeight(20)),
+        padding: EdgeInsets.only(top: getProportionateScreenHeight(20)),
         child: Container(
-              width: getProportionateScreenWidth(340),
-              height: getProportionateScreenHeight(600),
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
-              child:Column(
-                children: [
-                  _titulo(),
-                  Flexible(
-                    fit: FlexFit.tight,
-                    flex: 7,
-                    child: Container(
-                      decoration: BoxDecoration(color:Colors.grey[300],),
-                      child:Column(
-                        children: [
-                          Flexible(
-                            flex: 10,
-                              child: ListView.builder(
-                              itemCount: this.mensajes.length,
-                              itemBuilder: (BuildContext context, int index) {
+            width: getProportionateScreenWidth(340),
+            height: getProportionateScreenHeight(600),
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+            child: Column(
+              children: [
+                _titulo(),
+                Flexible(
+                  fit: FlexFit.tight,
+                  flex: 7,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                    ),
+                    child: Column(
+                      children: [
+                        Flexible(
+                          flex: 10,
+                          child: ListView.builder(
+                            itemCount: this.mensajes.length,
+                            itemBuilder: (BuildContext context, int index) {
                               return Container(
-                                child: MensajeView(mensaje:mensajes[index]),
+                                child: MensajeView(mensaje: mensajes[index]),
                               );
-                             },
+                            },
+                          ),
+                        ),
+                        Flexible(
+                          flex: 2,
+                          child: Container(
+                            child: FormInput(
+                              controller: mensajeController,
+                              hintText: "Ingrese mensaje",
+                              isContrasena: false,
+                              onSubmit: (string) async {
+                                Respuesta respuesta =
+                                    await RequestService.createMensaje(
+                                        widget.ordenActual.codigoOrden,
+                                        mensajeController.text);
+                                if (respuesta.error == "true") {
+                                  ShowMessage.showErrorDialog(
+                                      context, "Error", respuesta.respuesta);
+                                } else {
+                                  this.getMensajesNow();
+                                  mensajeController.clear();
+                                }
+                              },
                             ),
                           ),
-                          Flexible(
-                            flex: 2,
-                            child: Container(
-                              child: FormInput(
-                                controller:mensajeController, 
-                                hintText: "Ingrese mensaje",
-                                isContrasena: false,onSubmit: (string) async{
-                                  Respuesta respuesta= await RequestService.createMensaje(widget.ordenActual.codigoOrden, mensajeController.text);
-                                  if(respuesta.error=="true"){
-                                    ShowMessage.showErrorDialog(context, "Error", respuesta.respuesta);
-                                  }else{
-                                    this.getMensajesNow();
-                                    mensajeController.clear();
-                                  }
-                                },),
-                            ) ,
-                            )
-                        ],
-                      ),
+                        )
+                      ],
                     ),
-                    )
-                ],
-              )
-        ),
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
 
   Flexible _titulo() {
     return Flexible(
-      fit: FlexFit.tight,
-      flex:1,
-      child: Container(
-        decoration: BoxDecoration(color: kPrimaryColor),
-        child: Padding(
-          padding: EdgeInsets.only(left:getProportionateScreenHeight(20)),
-          child: Row(children: [
-            Container(
-              height: getProportionateScreenHeight(50),
-              width: getProportionateScreenHeight(50),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                image:  DecorationImage(
-                  image: NetworkImage(widget.ordenActual.cliente.imagen),
-                  fit: BoxFit.cover  
-                )
-                ),
-               
-            ),
-            Padding(
-              padding: EdgeInsets.only(left:getProportionateScreenWidth(10)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        fit: FlexFit.tight,
+        flex: 1,
+        child: Container(
+            decoration: BoxDecoration(color: kPrimaryColor),
+            child: Padding(
+              padding: EdgeInsets.only(left: getProportionateScreenHeight(20)),
+              child: Row(
                 children: [
-                Padding(
-                  padding:EdgeInsets.only(top:getProportionateScreenHeight(20)),
-                  child: Text("${widget.ordenActual.cliente.nombre} ${widget.ordenActual.cliente.apellido}",
-                    style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: getProportionateScreenHeight(20)),),
-                ),
-                Text(widget.ordenActual.cliente.direccion.direccion,style: TextStyle(fontSize: getProportionateScreenHeight(15)),)
-              ],),
-            )
-          ],),
-        )
-      )
-    );
+                  Container(
+                    height: getProportionateScreenHeight(50),
+                    width: getProportionateScreenHeight(50),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image:
+                                NetworkImage(widget.ordenActual.cliente.imagen),
+                            fit: BoxFit.cover)),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.only(left: getProportionateScreenWidth(10)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: getProportionateScreenHeight(20)),
+                          child: Text(
+                            "${widget.ordenActual.cliente.nombre} ${widget.ordenActual.cliente.apellido}",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: getProportionateScreenHeight(20)),
+                          ),
+                        ),
+                        Container(
+                            width: getProportionateScreenWidth(250),
+                            child: Text(
+                              widget.ordenActual.cliente.direccion.direccion,
+                              style: TextStyle(
+                                  fontSize: getProportionateScreenHeight(15)),
+                              maxLines: 1,
+                            ))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )));
   }
 
-  void getMensajes(Timer timer) async{
-     Respuesta respuesta= await RequestService.getMensajesOrden(widget.ordenActual.codigoOrden);
-     if(respuesta.error=="true"){
-       return;
-     }else{
-       List<Mensaje> mensajes=[];
-       for(var menJson in respuesta.respuesta){
-         Mensaje mensaje= Mensaje.fromJson(menJson);
-         mensajes.add(mensaje);
-       }
-       setState(() {
-         this.mensajes=mensajes;
-       });
-     }
+  void getMensajes(Timer timer) async {
+    Respuesta respuesta =
+        await RequestService.getMensajesOrden(widget.ordenActual.codigoOrden);
+    if (respuesta.error == "true") {
+      return;
+    } else {
+      List<Mensaje> mensajes = [];
+      for (var menJson in respuesta.respuesta) {
+        Mensaje mensaje = Mensaje.fromJson(menJson);
+        mensajes.add(mensaje);
+      }
+      setState(() {
+        this.mensajes = mensajes;
+      });
+    }
   }
 
-  void getMensajesNow() async{
-     Respuesta respuesta= await RequestService.getMensajesOrden(widget.ordenActual.codigoOrden);
-     if(respuesta.error=="true"){
-       return;
-     }else{
-       List<Mensaje> mensajes=[];
-       for(var menJson in respuesta.respuesta){
-         Mensaje mensaje= Mensaje.fromJson(menJson);
-         mensajes.add(mensaje);
-       }
-       setState(() {
-         this.mensajes=mensajes;
-       });
-     }
+  void getMensajesNow() async {
+    Respuesta respuesta =
+        await RequestService.getMensajesOrden(widget.ordenActual.codigoOrden);
+    if (respuesta.error == "true") {
+      return;
+    } else {
+      List<Mensaje> mensajes = [];
+      for (var menJson in respuesta.respuesta["mensajes"]) {
+        Mensaje mensaje = Mensaje.fromJson(menJson);
+        mensajes.add(mensaje);
+      }
+      setState(() {
+        this.mensajes = mensajes;
+      });
+    }
   }
-  
 }
